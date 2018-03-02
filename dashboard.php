e55395d18e406c229e7d3bdda928081f3356ecb3
zid1988 <zid1988@gmail.com>
2018-03-02 05:48:28 +0530

    nav sorted

---
+++ dashboard.php
@@ -0,0 +1,194 @@
+<?php
+
+  include_once('headers/header.php');
+
+?>
+
+
+
+<div class="page-wrap">
+  
+<?php
+
+  include_once('headers/nav.php');
+
+?>
+
+  
+
+  <div class="page-content">
+    
+<div class="container-fluid">
+  <div class="page-content__header">
+    <div>
+      <h2 class="page-content__header-heading">Dashboard</h2>
+      <div class="page-content__header-description">Hello, VA Talks!</div>
+    </div>
+    <!-- <div class="page-content__header-meta">
+      <a href="#" class="btn btn-info icon-left">
+        Add Widget <span class="btn-icon iconfont iconfont-plus-circle"></span>
+      </a>
+    </div> -->
+
+  </div>
+
+    <!--------------------------------- FACTS STARTS ------------------------------>
+
+  <div class="row">
+    <div class="col-xl-3 col-lg-3 col-md-6">
+      <div class="widget widget-alpha widget-alpha--color-amaranth">
+        <div>
+          <div class="widget-alpha__amount">278</div>
+          <div class="widget-alpha__description">New Projects</div>
+        </div>
+        <span class="widget-alpha__icon iconfont iconfont-bag-outline"></span>
+      </div>
+    </div>
+    <div class="col-xl-3 col-lg-3 col-md-6">
+      <div class="widget widget-alpha widget-alpha--color-green-jungle">
+        <div>
+          <div class="widget-alpha__amount">156</div>
+          <div class="widget-alpha__description">New Clients</div>
+        </div>
+        <span class="widget-alpha__icon iconfont iconfont-user-outline"></span>
+      </div>
+    </div>
+    <div class="col-xl-3 col-lg-3 col-md-6">
+      <div class="widget widget-alpha widget-alpha--color-orange widget-alpha--donut">
+        <div>
+          <div class="widget-alpha__amount">64,87%</div>
+          <div class="widget-alpha__description">Conversion Rate</div>
+        </div>
+        <span class="widget-alpha__icon iconfont iconfont-chart-donut-outline"></span>
+      </div>
+    </div>
+    <div class="col-xl-3 col-lg-3 col-md-6">
+      <div class="widget widget-alpha widget-alpha--color-java widget-alpha--help">
+        <div>
+          <div class="widget-alpha__amount">425</div>
+          <div class="widget-alpha__description">Support Tickets</div>
+        </div>
+        <span class="widget-alpha__icon iconfont iconfont-help-outline"></span>
+      </div>
+    </div>
+  </div>
+  <!----------------------------------- FACTS ENDS ------------------------------>
+
+  <div class="row">
+    <div class="col-lg-12">
+      <div class="main-container table-container">
+        <table class="table table__actions">
+          <thead>
+          <tr>
+            <th>Task ID</th>
+            <th>Cx Name</th>
+            <th>Subject</th>
+            <th>Status</th>
+            <th>Assigned</th>
+            <th>Action</th>
+          </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                <div class="table__cell-widget">
+                  <a href="#" class="table__cell-widget-name">1</a>
+                  <!-- <span class="table__cell-widget-description">Google</span> -->
+                </div>
+              </td>
+              <td>
+                <div class="table__cell-widget">
+                  <span class="table__cell-widget-name">Steven Cohen</span>
+                  <span class="table__cell-widget-description">Full Time</span>
+                </div>
+              </td>
+              <td>
+                <div class="table__cell-widget">
+                  <span class="table__cell-widget-name">Social Media Posting</span>
+                  <span class="table__cell-widget-description color-danger">URGENT</span>
+                </div>
+              </td>
+              <td class="table__label"><span class="badge badge-success">OPEN</span></td>
+              <td>
+                <div class="table__cell-user">
+                  <img src="img/users/user-10.png" alt="" class="table__cell-user-avatar rounded-circle">
+                  <div class="table__cell-user-wrap">
+                    <a href="#" class="table__cell-widget-name">Annie Cooper</a>
+                    <span class="table__cell-widget-description">UK, Design Team</span>
+                  </div>
+                </div>
+              </td>
+              <td class="table__cell-actions">
+                <div class="table__cell-actions-wrap">
+                  <div class="dropdown table__cell-actions-item">
+                    <button class="btn btn-secondary dropdown-toggle" type="button" data-toggle="dropdown">
+                      Actions
+                    </button>
+                    <div class="dropdown-menu">
+                      <a class="dropdown-item" href="#">Action</a>
+                      <a class="dropdown-item" href="#">Another action</a>
+                      <a class="dropdown-item" href="#">Something else here</a>
+                    </div>
+                  </div>
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <div class="table__cell-widget">
+                  <a href="#" class="table__cell-widget-name">2</a>
+                  <!-- <span class="table__cell-widget-description">Google</span> -->
+                </div>
+              </td>
+              <td>
+                <div class="table__cell-widget">
+                  <span class="table__cell-widget-name">Steven Cohen</span>
+                  <span class="table__cell-widget-description">Full Time</span>
+                </div>
+              </td>
+              <td>
+                <div class="table__cell-widget">
+                  <span class="table__cell-widget-name">Social Media Posting</span>
+                  <span class="table__cell-widget-description color-danger">URGENT</span>
+                </div>
+              </td>
+              <td class="table__label"><span class="badge badge-success">OPEN</span></td>
+              <td>
+                <div class="table__cell-user">
+                  <img src="img/users/user-10.png" alt="" class="table__cell-user-avatar rounded-circle">
+                  <div class="table__cell-user-wrap">
+                    <a href="#" class="table__cell-widget-name">Annie Cooper</a>
+                    <span class="table__cell-widget-description">UK, Design Team</span>
+                  </div>
+                </div>
+              </td>
+              <td class="table__cell-actions">
+                <div class="table__cell-actions-wrap">
+                  <div class="dropdown table__cell-actions-item">
+                    <button class="btn btn-secondary dropdown-toggle" type="button" data-toggle="dropdown">
+                      Actions
+                    </button>
+                    <div class="dropdown-menu">
+                      <a class="dropdown-item" href="#">Action</a>
+                      <a class="dropdown-item" href="#">Another action</a>
+                      <a class="dropdown-item" href="#">Something else here</a>
+                    </div>
+                  </div>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+
+</div>
+  
+
+
+<?php
+
+  include_once('headers/footer.php');
+
+?>
---
+++ headers/nav.php
@@ -0,0 +1,116 @@
+    <div class="sidebar">
+  <div class="sidebar__scroll">
+    <div>
+      <div class="sidebar__user">
+        <div class="sidebar__user-avatar">
+          <!--<img src="img/avatars/placeholder.png" alt="" width="68" height="68" class="rounded-circle">-->
+          <span class="sidebar__user-avatar-placeholder">
+            <span class="iconfont iconfont-avatar-placeholder"></span>
+          </span>
+          <!--<img src="img/users/user-1.jpg" alt="" width="68" height="68" class="rounded-circle">-->
+        </div>
+        <div class="dropdown sidebar__user-dropdown">
+          <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Alexander Porter
+          </a>
+          <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+            <a class="dropdown-item" href="#">Settings</a>
+            <a class="dropdown-item" href="#">Help</a>
+            <a class="dropdown-item" href="#">Sign Out</a>
+          </div>
+        </div>
+      </div>
+      <ul class="sidebar-nav">
+        <li class="sidebar-nav__item">
+          <a class="sidebar-nav__link" href="dashboard.php">
+            <span class="sidebar-nav__item_icon iconfont iconfont-home"></span>
+            <span class="sidebar-nav__item-text">Dashboard</span>
+          </a>
+        </li>
+        <li class="sidebar-nav__item">
+          <a class="sidebar-nav__link" href="#">
+            <span class="sidebar-nav__item_icon iconfont iconfont-ui"></span>
+            <span class="sidebar-nav__item-text">Tasks</span>
+          </a>
+          <ul class="sidebar-subnav">
+            <li class="sidebar-subnav__item"><a class="sidebar-subnav__link" href="addTask.php">Add Task</a></li>
+            <li class="sidebar-subnav__item"><a class="sidebar-subnav__link" href="allTasks.php">All Tasks</a></li>
+            <li class="sidebar-subnav__item"><a class="sidebar-subnav__link" href="unrecognized.php">Unrecognized Emails</a></li>
+          </ul>
+        </li>
+        <li class="sidebar-nav__item">
+          <a class="sidebar-nav__link" href="#">
+            <span class="sidebar-nav__item_icon iconfont iconfont-ui"></span>
+            <span class="sidebar-nav__item-text">Customers</span>
+          </a>
+          <ul class="sidebar-subnav">
+            <li class="sidebar-subnav__item"><a class="sidebar-subnav__link" href="addAccount.php">Add Account</a></li>
+            <li class="sidebar-subnav__item"><a class="sidebar-subnav__link" href="activeAcounts.php">Active Accounts</a></li>
+            <li class="sidebar-subnav__item"><a class="sidebar-subnav__link" href="allAccounts.php">View All Accounts</a></li>
+          </ul>
+        </li>
+        <li class="sidebar-nav__item">
+          <a class="sidebar-nav__link" href="#">
+            <span class="sidebar-nav__item_icon iconfont iconfont-input"></span>
+            <span class="sidebar-nav__item-text">Employees</span>
+          </a>
+          <ul class="sidebar-subnav">
+            <li class="sidebar-subnav__item"><a class="sidebar-subnav__link" href="addEmployee.php">Add New</a></li>
+            <li class="sidebar-subnav__item"><a class="sidebar-subnav__link" href="activeEmployees.php">Active Employees</a></li>
+            <li class="sidebar-subnav__item"><a class="sidebar-subnav__link" href="allEmployees.php">View All Employees</a></li>
+          </ul>
+        </li>
+        <li class="sidebar-nav__item">
+          <a class="sidebar-nav__link" href="#">
+            <span class="sidebar-nav__item_icon iconfont iconfont-component"></span>
+            <span class="sidebar-nav__item-text">Email Templates</span>
+          </a>
+          <ul class="sidebar-subnav">
+            <li class="sidebar-subnav__item"><a class="sidebar-subnav__link" href="addTemplate.php">Add New</a></li>
+            <li class="sidebar-subnav__item"><a class="sidebar-subnav__link" href="viewTemplates.php">View Templates</a></li>
+          </ul>
+        </li>
+        <li class="sidebar-nav__item">
+          <a class="sidebar-nav__link" href="#">
+            <span class="sidebar-nav__item_icon iconfont iconfont-pages"></span>
+            <span class="sidebar-nav__item-text">Coupons</span>
+          </a>
+          <ul class="sidebar-subnav">
+            <li class="sidebar-subnav__item"><a class="sidebar-subnav__link" href="createCoupon.php">Create Coupon</a></li>
+            <li class="sidebar-subnav__item"><a class="sidebar-subnav__link" href="manageCoupons.php">Manage Coupons</a></li>
+          </ul>
+        </li>
+        <li class="sidebar-nav__item">
+          <a class="sidebar-nav__link" href="helpCenter.php">
+            <span class="sidebar-nav__item_icon iconfont iconfont-help-circle"></span>
+            <span class="sidebar-nav__item-text">Help Center</span>
+          </a>
+          <!-- <ul class="sidebar-subnav">
+            <li class="sidebar-subnav__item"><a class="sidebar-subnav__link" href="help-center.html">Browse Help Desk</a></li>
+            <li class="sidebar-subnav__item"><a class="sidebar-subnav__link" href="help-center-submit-ticket.html">Submit Ticket</a></li>
+          </ul> -->
+        </li>
+        <li class="sidebar-nav__item">
+          <a class="sidebar-nav__link" href="settings.php">
+            <span class="sidebar-nav__item_icon iconfont iconfont-settings"></span>
+            <span class="sidebar-nav__item-text">Settings</span>
+          </a>
+          <!-- <ul class="sidebar-subnav">
+            <li class="sidebar-subnav__item"><a class="sidebar-subnav__link" href="app-settings.html">App Settings</a></li>
+            <li class="sidebar-subnav__item"><a class="sidebar-subnav__link" href="global-settings.html">Global Settings</a></li>
+            <li class="sidebar-subnav__item"><a class="sidebar-subnav__link" href="account-settings.html">Account Settings</a></li>
+          </ul> -->
+        </li>
+        <li class="sidebar-nav__item">
+          <span class="sidebar-nav__link sidebar-nav__source">&copy; 2018 VA Talks. ver. 3.0 beta</span>
+        </li>
+      </ul>
+    </div>
+
+    <div class="sidebar-nav__footer">
+      <div class="sidebar__collapse">
+        <span class="icon iconfont iconfont-collapse-left-arrows"></span>
+      </div>
+    </div>
+  </div>
+</div>
